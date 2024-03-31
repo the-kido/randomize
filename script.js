3b66e12016ac417bc89beacba291a344cfd92b19
Kido <63456333+the-kido@users.noreply.github.com>
2024-03-31 13:05:25 -0400

    I beutified everything and now it looks great

--- script.js
+++ script.js
@@ -1,78 +1,140 @@
-(color) =>`.center { border: 3px solid ` + color + `; }`
-
 const text = document.getElementById("text")
 const button = document.getElementById("button");
 const root = document.querySelector(':root');
+const mainDiv = document.getElementsByClassName("center")[0]
 
+
+const properties = {
+    borderColor: "--border-color",
+    backgroundColor: "--background-color",
+    direction: "--direction",
+    previousDirection: "--previous-direction", 
+    transitionTime: "--transition-time"
+}
+
+// Averaging the colours allows for a more natural feel
+const averageBias = 0.5;
+const animationTime = parseFloat(getComputedStyle(root).getPropertyValue(properties.transitionTime).replace("s", "")) * 1000;
+// Invoke right as website is opened.
 onButtonPressed();
+
 function onButtonPressed() {
-    var r = Math.random()*200,
-        g = Math.random()*200,
-        b = Math.random()*200;
-        
-        var newBorderColor = "rgb(" + [r, g, b].join(", "); + ")"
-        root.style.setProperty("--border-color", newBorderColor);
-        
-        var hsv = rgb2hsv(r, g, b)
-        hsv.h = Math.round(hsv.h, 2) - 25;
-        hsv.s = Math.round(hsv.s, 2) / 5+ "%";
-        
-        hsv.v = Math.max(100 - hsv.v, hsv.v) * 1.5;
-        hsv.v = hsv.v > 100 ? "90%" : hsv.v + "%";
-        
-        var newBackgroundColor = "hsl(" + hsv.h + ", " + hsv.s + ", " + hsv.v + ")"
-        root.style.setProperty("--background-color", newBackgroundColor);
-        
-        var rand = Math.random()
-        text.innerHTML = rand <= 0.5 ? "←" : "→"
-    }
+    // Debounce
+    if (!inputAcceptable()) return;
 
-    var div = document.getElementsByClassName("center")[0]
+    var r = Math.random() * 130 + 60,
+        g = Math.random() * 130 + 60,
+        b = Math.random() * 130 + 60;
 
-    const myObserver = new ResizeObserver(() => {
-        console.log("hello " + document.body.scrollHeight)
-        console.log("hello " + div.offsetHeight + " also")
-        var height = parseInt( (document.body.scrollHeight - div.offsetHeight) / 2 );
-        div.style.marginTop = height.toString() + "px";
-    }).observe(document.body);
+    updateBorderColor(r, g, b);
+    updateBackgroundColor(r, g, b);
+    flipArrow();
+}
 
+var debounce = true;
+function inputAcceptable() {
+    if (debounce === true) return false;
 
-// stolen: https://stackoverflow.com/a/54070620
+    debounce = true;
+    setTimeout(
+        () => {
+            debounce = false;    
+        }, 
+        animationTime
+    );
+
+    return true;
+}
+
+function updateBorderColor(r, g, b) {
+    let complementBias = 1 - averageBias
+    let avg = (r + g + b) / 3
+    
+    r = r * complementBias + avg * averageBias
+    g = g * complementBias + avg * averageBias
+    b = b * complementBias + avg * averageBias
+    
+    var newBorderColor = "rgb(" + [r, g, b].join(", ") + ")"
+    root.style.setProperty(properties.borderColor, newBorderColor);
+}
+
+function mod(value, mod) {
+    return (value % mod + mod) % mod;
+}
+
+function updateBackgroundColor(r, g, b) {
+    var hsv = rgb2hsv(r, g, b)
+    
+    // Fix up hue, saturation, and value to make it a good background color
+    hsv.h = mod((Math.round(hsv.h, 2) - 120), 360);
+    hsv.s = Math.round(hsv.s, 2) / 2 + "%";
+    hsv.v = Math.max(100 - hsv.v, hsv.v) * 1.5;
+    hsv.v = hsv.v > 100 ? "90%" : hsv.v + "%";
+    
+    var newBackgroundColor = "hsl(" + hsv.h + ", " + hsv.s + ", " + hsv.v + ")"
+    root.style.setProperty(properties.backgroundColor, newBackgroundColor);
+}
+
+function flipArrow() {
+    // Set the previous direction
+    root.style.setProperty(
+        properties.previousDirection,     
+        getComputedStyle(root).getPropertyValue(properties.direction)
+    );
+    // Update new direction for CSS animation to use.
+    root.style.setProperty(properties.direction, Math.random() <= 0.5 ? 1 : -1);
+
+    // Play the wobble animation and delete it after it is done..
+    text.classList.add("wobble")
+    setTimeout(
+        () => text.classList.remove('wobble'), 
+        animationTime
+    );
+}
+
+function updateTMP() {
+    mainDiv.style.marginTop = 0;
+    var height = parseInt((document.body.scrollHeight - mainDiv.offsetHeight) / 2);
+    mainDiv.style.marginTop = height.toString() + "px";
+}
+
+const myObserver = new ResizeObserver(updateTMP).observe(mainDiv);
+
+// Stolen: https://stackoverflow.com/a/8023734. Tyvm for letting me not learn this conversion nonsense
 function rgb2hsv (r, g, b) {
-    let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
+    let rabs, gabs, babs, rr, gg, bb, hue, saturation, value, diff, diffc, percentRoundFn;
+    
     rabs = r / 255;
     gabs = g / 255;
     babs = b / 255;
-    v = Math.max(rabs, gabs, babs),
-    diff = v - Math.min(rabs, gabs, babs);
-    diffc = c => (v - c) / 6 / diff + 1 / 2;
+    value = Math.max(rabs, gabs, babs),
+    diff = value - Math.min(rabs, gabs, babs);
+    diffc = c => (value - c) / 6 / diff + 1 / 2;
     percentRoundFn = num => Math.round(num * 100) / 100;
     if (diff == 0) {
-        h = s = 0;
+        hue = saturation = 0;
     } else {
-        s = diff / v;
+        saturation = diff / value;
         rr = diffc(rabs);
         gg = diffc(gabs);
         bb = diffc(babs);
 
-        if (rabs === v) {
-            h = bb - gg;
-        } else if (gabs === v) {
-            h = (1 / 3) + rr - bb;
-        } else if (babs === v) {
-            h = (2 / 3) + gg - rr;
+        if (rabs === value) {
+            hue = bb - gg;
+        } else if (gabs === value) {
+            hue = (1 / 3) + rr - bb;
+        } else if (babs === value) {
+            hue = (2 / 3) + gg - rr;
         }
-        if (h < 0) {
-            h += 1;
-        }else if (h > 1) {
-            h -= 1;
+        if (hue < 0) {
+            hue += 1;
+        } else if (hue > 1) {
+            hue -= 1;
         }
     }
     return {
-        h: Math.round(h * 360),
-        s: percentRoundFn(s * 100),
-        v: percentRoundFn(v * 100)
+        h: Math.round(hue * 360),
+        s: percentRoundFn(saturation * 100),
+        v: percentRoundFn(value * 100)
     };
-
-    
 }
